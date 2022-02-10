28f68288968989a00d77ab0c1bd080f32479986b
mateus <mateuslourenco55@outlook.com>
2022-02-09 23:57:11 -0300

    adicionado pacote python-decouple
    configurado variavel DEBUG

--- quiz/settings.py
+++ quiz/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qs3i@ez3_n%z&2cp*mv%=gh14#+cwi5f4!+3y6c56wpgjr85)5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
