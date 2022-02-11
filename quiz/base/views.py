79d913d7f3f046b4a23fcb69d4c2ea05965c5524
mateus <mateuslourenco55@outlook.com>
2022-02-11 20:01:58 -0300

    implementada classificacao de alunos
    close #11

--- quiz/base/views.py
+++ quiz/base/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Sum
 from django.shortcuts import render, redirect
 from django.utils.timezone import now
 
@@ -28,7 +29,23 @@ def home(request):
 
 
 def classificacao(request):
-    return render(request, 'base/classificacao.html')
+    try:
+        aluno_id = request.session['aluno_id']
+    except KeyError:
+        return redirect('/')
+    else:
+        pontos_dct = Resposta.objects.filter(aluno_id=aluno_id).aggregate(Sum('pontos'))
+        pontuacao_do_aluno = pontos_dct['pontos__sum']
+        numero_de_alunos_com_maior_pontuacao = Resposta.objects.values('aluno').annotate(Sum('pontos')).filter(pontos__sum__gt=pontuacao_do_aluno).count()
+        primeiros_alunos_da_classificacao = list(
+            Resposta.objects.values('aluno', 'aluno__nome').annotate(Sum('pontos')).order_by('-pontos__sum')[:5]
+        )
+        ctx = {
+            'pontuacao_do_aluno': pontuacao_do_aluno,
+            'posicao_do_aluno': numero_de_alunos_com_maior_pontuacao + 1,
+            'primeiros_alunos_da_classificacao': primeiros_alunos_da_classificacao,
+        }
+        return render(request, 'base/classificacao.html', ctx)
 
 
 PONTUACAO_MAXIMA = 1000
