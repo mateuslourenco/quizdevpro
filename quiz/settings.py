8769f9fefd842921cf5315909c6929a3c8a33e18
mateus <mateuslourenco55@outlook.com>
2022-02-11 23:42:01 -0300

    configurado secret key

--- quiz/settings.py
+++ quiz/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qs3i@ez3_n%z&2cp*mv%=gh14#+cwi5f4!+3y6c56wpgjr85)5'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
