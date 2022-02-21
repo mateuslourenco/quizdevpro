80c083869524d6ed53518fa60fb1cc512b76911c
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-02-21 20:17:04 -0300

    implementado teste inicial para pergunta existente

--- quiz/base/tests/test_classificacao.py
+++ quiz/base/tests/test_classificacao.py
@@ -1,6 +1,8 @@
 import pytest
 from django.urls import reverse
 
+from quiz.base.models import Aluno, Pergunta
+
 
 @pytest.fixture
 def resp(client):
@@ -10,12 +12,16 @@ def resp(client):
 def test_status_code(resp):
     assert resp.status_code == 302
     assert resp.url == reverse('home')
-#
-#
-# @pytest.fixture
-# def resp_com_aluno_logado(client_com_aluno_logado, aluno, pergunta):
-#     return client_com_aluno_logado.get(reverse('classificacao'))
-#
-#
-# def test_status_code_aluno_logado(resp_com_aluno_logado):
-#     assert resp_com_aluno_logado.status_code == 200
+
+
+@pytest.fixture
+def resp_com_aluno_logado(client_com_aluno_logado, pergunta, resposta):
+    return client_com_aluno_logado.get(reverse('classificacao'))
+
+
+def test_status_code_aluno_logado(resp_com_aluno_logado):
+    assert resp_com_aluno_logado.status_code == 200
+
+
+def test_pergunta_existe_no_db(db, resp_com_aluno_logado):
+    assert Pergunta.objects.exists()
