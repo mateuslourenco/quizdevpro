4e11c59510e2272da99a9a4a8f0b478b7ea5fc2a
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-02-21 16:02:04 -0300

    implementado teste home post

--- quiz/tests/test_home.py
+++ quiz/tests/test_home.py
@@ -1,5 +1,8 @@
 import pytest
 from django.urls import reverse
+from model_mommy import mommy
+
+from quiz.base.models import Aluno
 
 
 @pytest.fixture
@@ -9,3 +12,19 @@ def resp(client):
 
 def test_status_code(resp):
     assert resp.status_code == 200
+
+
+@pytest.fixture
+def aluno(db):
+    aluno_modelo = mommy.make(Aluno)
+    return aluno_modelo
+
+
+@pytest.fixture
+def resp_post(client, aluno):
+    return client.post(reverse('home'), {'nome': aluno.nome, 'email': aluno.email})
+
+
+def test_aluno_existente_redirect(resp_post):
+    assert resp_post.status_code == 302
+    assert resp_post.url == '/perguntas/1'
