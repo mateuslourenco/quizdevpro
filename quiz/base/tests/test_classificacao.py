83d4ffc4392c4f702f5fbf8113f23435e0afd2ba
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-02-21 20:25:46 -0300

    finalizado testes classificacao
    close #39

--- quiz/base/tests/test_classificacao.py
+++ quiz/base/tests/test_classificacao.py
@@ -1,7 +1,8 @@
 import pytest
+from django.db.models import Sum
 from django.urls import reverse
 
-from quiz.base.models import Aluno, Pergunta
+from quiz.base.models import Aluno, Pergunta, Resposta
 from quiz.django_assertions import assert_contains
 
 
@@ -30,3 +31,13 @@ def test_pergunta_existe_no_db(db, resp_com_aluno_logado):
 
 def test_ranking_presente(resp_com_aluno_logado):
     assert_contains(resp_com_aluno_logado, '<h3>Ranking')
+
+
+def test_nome_aluno_presente(resp_com_aluno_logado, aluno):
+    assert_contains(resp_com_aluno_logado, aluno.nome)
+
+
+def test_soma_de_pontos_presente(resp_com_aluno_logado, aluno, resposta):
+    pontos_dct = Resposta.objects.filter(aluno_id=aluno.id).aggregate(Sum('pontos'))
+    pontos = pontos_dct['pontos__sum']
+    assert_contains(resp_com_aluno_logado, pontos)
