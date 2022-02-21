5b00c042e8281c3bf48c8cad1ec61f525a8cb42b
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-02-21 20:17:04 -0300

    implementado teste inicial para pergunta existente

--- quiz/base/conftest.py
+++ quiz/base/conftest.py
@@ -6,20 +6,19 @@ from quiz.base.models import Aluno, Pergunta, Resposta
 
 @pytest.fixture
 def aluno(db):
-    aluno = baker.make(Aluno)
-    return aluno
+    return baker.make(Aluno)
 
 
 @pytest.fixture
 def pergunta(db):
     alternativas = ['alternativa1', 'alternativa2', 'alternativa3', 'alternativa4']
-    pergunta = baker.make(Pergunta, disponivel=True, alternativas=alternativas)
+    pergunta = baker.make(Pergunta, disponivel=True, alternativas={'array': alternativas})
     return pergunta
 
 
 @pytest.fixture
 def resposta(aluno, pergunta):
-    resposta = baker.make(Resposta, aluno=aluno, pergunta=pergunta, pontos=200)
+    resposta = baker.make(Resposta, aluno=aluno, pergunta=pergunta, pontos=1000)
     return resposta
 
 
