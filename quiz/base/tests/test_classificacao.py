4e93d9b5b910e76d03b9484a46e344b299501497
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-02-21 20:20:52 -0300

    implementado teste para validar ranking existente

--- quiz/base/tests/test_classificacao.py
+++ quiz/base/tests/test_classificacao.py
@@ -2,6 +2,7 @@ import pytest
 from django.urls import reverse
 
 from quiz.base.models import Aluno, Pergunta
+from quiz.django_assertions import assert_contains
 
 
 @pytest.fixture
@@ -25,3 +26,7 @@ def test_status_code_aluno_logado(resp_com_aluno_logado):
 
 def test_pergunta_existe_no_db(db, resp_com_aluno_logado):
     assert Pergunta.objects.exists()
+
+
+def test_ranking_presente(resp_com_aluno_logado):
+    assert_contains(resp_com_aluno_logado, '<h3>Ranking')
